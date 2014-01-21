d79102841254f21896e740ee099cd4c2a3728e2c
Nate Merrill <nateme16@gmail.com>
2014-01-21 11:51:15 -0500

    added interpolation code

--- groundwatertodd_interp.m
+++ groundwatertodd_interp.m
@@ -29,17 +29,17 @@ Gamma = @(x) x + (1/(A*S))*rec;
 u1 = @(w,x)  (((((r+w).^2)./2.*k) - ((g.*(r+w))./k) - (c0+c1.*x)*w)).*alpha(x) + A-alpha(x).*((r.^2)/(2.*k.* - (g.*r)./k)) ; % profit from water pumped (1 period)
 %u2= @(x)  (r.^2)/2.*k.*alpha - (g.*r)./k  dyrland only
 
-%w=[0:.01:1]  %test of value function;;
-%x=100;
-%u1(w,x);
-%plot(w,ans);
+w=[0:.01:1]  %test of value function;;
+x=100;
+u1(w,x);
+plot(w,ans);
 
 
 %% Value function iteration parameters
 tol = 1e-4; % convergence tolerance
 maxit = 500; % maximum number of loop iterations
 
-n =500; % n specifies how fine our grid of capital holdings will be
+n =2000; % n specifies how fine our grid of capital holdings will be
 X = linspace(min_k,max_k,n); % an evenly spaced grid
 
 % pre-compute the return function on the entire grid of states and possible
@@ -100,38 +100,38 @@ for b=1:n;
 end;
 
 
+
+
+
+valuefit=fit(X',v,'cubicinterp')  % interpolates cubic function to value function
+
+
+
+valueint=@(w,x) -(u1(w,x)+beta*valuefit((x + (rec+(re-1).*w.*alpha(x))./(A.*S)))) % this is the function to optimize
+
+for i=1:n; %loops over water levels
+    
+    x=X(i);
+    
+policyint(i)= fminsearch(@(w)valueint(w,x),.01); % calculates optimal policy at levels
+
+end
+
+toc/60
+
+
 %% Show the results
-subplot (2, 1, 1);
-plot(X,v); % plot results
+
+subplot (3, 1, 1);
+plot(X,valuefit(X)); % plot results
 title('Optimal value for current elevation (from sea level)');
 xlabel('Water Table Elevation');
 ylabel('Present discounted value');
 
-subplot (2, 1, 2);
-plot(X,policy)
+subplot (3, 1, 2);
+plot(X,policyint)
 
 
-
-toc/60
-
-xq=0:1:n;
-vq1 = interp1(X,v,xq);
-plot(X,v,'o',xq,vq1);
-title('(Default) Linear Interpolation');
-
-
-
-valueint=@(w,x) -(u1(w,x)+beta*interpvalue( (x - (rec+(re-1).*w.*alpha(x))./(A.*S)),v,X,n))
-
-for i=1:n;
-    
-    x=X(i);
-    
-policyint(i)= fminsearch(@(w)valueint(w,x),0);
-
-end
-
-plot(policyint)
-
-
+%subplot (3, 1, 3);
+%plot(X,policy)
 
