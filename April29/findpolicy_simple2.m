1af115c116072a06ba93224f69a938875363c540
Nate Merrill <nateme16@gmail.com>
2014-04-29 15:33:29 -0400

    end of day

--- April29/findpolicy_simple2.m
+++ April29/findpolicy_simple2.m
@@ -2,10 +2,10 @@
 %This function takes inputs from iteration file and returns optimal value
 % and policy functions
 
-function [policyopt valuefit policy policyint v X u1 ] = findpolicy_simple2(n,beta,r,k,g,c0,c1,A,rec,S,re,max_k,min_k,tol,maxit)
+function [policy v X R wp] = findpolicy_simple2(n,beta,r,k,g,c0,c1,A,rec,S,re,max_k,min_k,tol,maxit)
 
 %% Define payoff space (returns to pumping choice)
-Gamma =@(x) x + eom2(rec,re,0,irrig(A,max_k,min_k,x),S) %limit next period's water levels
+Gamma =@(x) x + eom2(rec,re,0,irrig(A,max_k,min_k,x),S); %limit next period's water levels
 
 X = linspace(min_k,max_k,n); % an evenly spaced grid over water levels
 
@@ -22,18 +22,41 @@ for i = 1:n % loop over the water states;
         % check to see if next period's water choice is feasible
         if(y <= Gamma(x)&&y>= min_k && y<= max_k);
             % if so, set the appropriate return and corresponding policy 
-        wp(i,j)= (((y-x) + eom2(rec,re,0,irrig(A,max_k,min_k,x),S)).*(irrig(A,max_k,min_k,x)*S) )./irrig(A,max_k,min_k,x) ;  % Policy corresponding to grid space
-        pi(i,j)=u12(wp(i,j),r,k,g,c0,c1,A,rec,S,re,max_k,min_k,irrig(A,max_k,min_k,x),x); %profit from choice of policy
+        wp(i,j)= (((y-x) + eom2(rec,re,0,irrig(A,max_k,min_k,x),S)).*(irrig(A,max_k,min_k,x)*S) )./irrig(A,max_k,min_k,x) ;  % Policy (per irrigated acre) corresponding to grid space
+        R(i,j)=u12(wp(i,j),r,k,g,c0,c1,A,rec,S,re,max_k,min_k,irrig(A,max_k,min_k,x),x); %profit from choice of policy
         end
     end
 end
 
 
+%% Value function iteration loop
+v = zeros(n,1); % initialize value function "guess" to zeros
+tv = v; % pre-allocate space for the updated value function
 
+% loop until either we converge or we hit the maximum number of iterations
 
+for i=1:maxit
+  
+    for j=1:n  % loop over all possible water states
+        % use the bellman mapping T(v) to map v into tv
+        [tv(j) I(j)] = max(R(j,:) + beta.* v');
+        policy(j)= wp(j,I(j));
+    end
+    
+    % compute the "distance" between the old and new value functions
+    diff = max(abs(tv - v));
+    fprintf('Iteration %3d: %.6f\n',i,diff);
 
-
-
+    % check for convergence
+    if(diff < tol);
+        fprintf('Value function iteration has converged.\n');
+        break;
+    end
+    
+    % update the old value function values with the new ones
+    v = tv;
+    
+end
 
 
 end
